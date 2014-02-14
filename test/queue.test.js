bea20c227e1584feb235f2ba2c74524f740c7276
Kaique da Silva <kaique.developer@gmail.com>
2014-02-14 04:44:42 -0200

    Queue
    default tasks was added:
    - initializer
    - mkdir
    - copy
    * template
    * sub
    * shell
    * make

--- test/queue.test.js
+++ test/queue.test.js
@@ -1,6 +1,8 @@
+'use strict';
 var ether = require('../');
 var queue = require('../lib/queue');
 var assert = require('assert');
+var fs = require('fs');
 
 describe('Ether', function () {
   var context = {
@@ -11,5 +13,31 @@ describe('Ether', function () {
   it('app banner', function (done) {
     app.run('initializer');
     done();
+  });
+
+  it('app mkdir', function (done) {
+    app.run('mkdir', [
+      './test/case/dst',
+    ]);
+    var expected = fs.existsSync('./test/case/dst');
+
+    assert.ok(expected);
+    done();
+  });
+
+  it('app copy', function (done) {
+    app.run('copy', [
+      './test/case/src/copy.txt',
+      './test/case/dst/copy.txt'
+    ]);
+    var expected = fs.existsSync('./test/case/dst/copy.txt');
+
+    assert.ok(expected);
+    fs.unlinkSync('./test/case/dst/copy.txt');
+    done();
+  });
+
+  after(function () {
+    fs.rmdirSync('./test/case/dst');
   });
 });
