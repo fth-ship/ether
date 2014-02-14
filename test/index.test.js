2741b04aa32384d656543bda9fd5e148ef151982
Kaique da Silva <kaique.developer@gmail.com>
2014-02-14 03:29:37 -0200

    Ether
    get and set properties of configuration
    register and run tasks

---
+++ test/index.test.js
@@ -0,0 +1,45 @@
+var ether = require('../');
+var assert = require('assert');
+
+describe('Ether', function () {
+  var app = ether({
+    name: 'TestCase'
+  });
+
+  it('app get name', function () {
+    var actual = app.get('name');
+    var expected = (actual === 'TestCase');
+
+    assert.ok(expected);
+  });
+
+  it('app set version', function () {
+    app.set('version', '0.0.1');
+    var actual = app.get('version');
+    var expected = (actual === '0.0.1');
+
+    assert.ok(expected);
+  });
+
+  it('app has version', function () {
+    var actual = app.has('version');
+
+    assert.ok(actual);
+  });
+
+  it('app has not tree', function () {
+    var actual = app.has('tree');
+
+    assert.ok(!actual);
+  });
+
+  it('register a task', function () {
+    app.task('sum', function (a, b) {
+      return a + b;
+    });
+    var actual = app.run('sum', [1, 2]);
+    var expected = (actual === 3);
+
+    assert.ok(expected);
+  });
+});
