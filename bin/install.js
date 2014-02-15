f2d1f846933c3d7e07c293b7c722056fa7afc06b
Kaique da Silva <kaique.developer@gmail.com>
2014-02-15 17:03:40 -0200

    Solving conditional to load file or module

--- bin/install.js
+++ bin/install.js
@@ -5,7 +5,10 @@ function binInstallHandler(filepath) {
   var self = this;
   var filepath = (filepath || 'etherfile.js');
   var fileLocation = path.resolve(self.cwd, filepath);
-  var load = require(fileLocation);
+  var hasFileLocation = (fs.existsSync(fileLocation));
+  var load = (hasFileLocation ? fileLocation : filePath);
+
+  load = require(load);
 
   load
     .run('initializer')
